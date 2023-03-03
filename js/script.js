ff1523b1f128dc1464397cee4e2b8721fbe0c0f1
sollendy <edlannino@tutanota.com>
2023-03-03 15:04:17 +0100

    esercizio in preparazione. redazione di pseudocodice

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//voglio creare una griglia di gioco numerata da 1 a 100 che spunta non appena l'utente preme il bottone
+//creo gli elementi html del contenitore del contenitore della griglia e del bottone
+//creo le variabili del contenitore e del bottone
+//creo le variabili del numero di celle nella griglia
+//creo gli elementi uno alla volta in un ciclo for
+//creo la funzione in cui redigere le istruzioni da dare al click del bottone
+//--> nel click voglio che:
+    //si crei un nuovo elemento che sarà un div 
